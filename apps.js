81baefbbc0650bead81c6fdf3fbeda61754c296e
HananAlmomani <hananalmomani11@yahoo.com>
2021-03-07 12:33:17 +0200

    added file

---
+++ apps.js
@@ -0,0 +1,11 @@
+var names = prompt("Enter Your name ")
+
+document.write("Hello "+ names);
+var makeupBrand = prompt("what brand do you want to look for ?");
+
+while (makeupBrand != 'MN' && makeupBrand != 'sephora')
+{
+    makeupBrand = promot ("what brand do you want to look for ?");
+    
+}
+
